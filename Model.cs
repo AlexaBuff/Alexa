816f0315dd726ee883571e1e05a7b855d232c855
Alexa <87079059+AlexaBuff@users.noreply.github.com>
2021-08-31 12:34:11 +1000

    Add files via upload

---
+++ Model.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleITunes
+{
+   public class Model : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }      
+
+        public string DbPath { get; private set; }       
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+
+            
+            DbPath = $"{AppDomain.CurrentDomain.BaseDirectory}blogging.db";
+            options.UseSqlite($"Data Source={DbPath}");
+        }
+    }
+
+    public class Album
+    {
+        public int Id { get; set; }
+        public long AlbumArtistID { get; set; }
+        public string ArtistName { get; set; }
+        public string AlbumName { get; set; }    
+
+        public DateTime DateRelise { get; set; }
+
+       
+    }
+
+   
+}
---
+++ Program.cs
@@ -0,0 +1,166 @@
+﻿using iTunesSearch.Library;
+using iTunesSearch.Library.Models;
+using System;
+using System.Linq;
+using System.Globalization;
+using System.Net.Sockets;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Timers;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using System.Threading;
+using System.Net.Http;
+
+namespace ConsoleITunes
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Model db = new Model();
+            db.Database.EnsureCreated();
+
+            iTunesSearchManager search = new iTunesSearchManager();
+            string inputArtist = "";
+            do
+            {
+                try
+                {
+                    Console.WriteLine("     Давайте найдём Вашего артиста на iTunes:(наберите текст и нажмите Enter) ");
+                    inputArtist = Console.ReadLine();
+                    Console.WriteLine("     поиск...");
+
+                    var artists = search.GetSongArtistsAsync(inputArtist).Result;
+
+                    var artistsSortAsc = from artistsSort in artists.Artists
+                                         orderby artistsSort.ArtistName
+                                         select artistsSort;
+                    int artistsCount = artistsSortAsc.Count();
+                    if (artistsCount == 0) Console.WriteLine("      iTunes поискал и не нашёл такого артиста! ");
+
+                    else
+                    {
+                        Console.WriteLine("     iTunes поискал и нашёл: ");
+                        SeachArtist(artistsSortAsc, search, db);
+                    }
+                }
+                catch (Exception e)
+                {
+
+                    if (!CheckConnection())
+                    {
+
+                        Cache(inputArtist, db);
+                    }
+                    else
+                    {
+                        Console.WriteLine(e.Message);
+                        continue;
+                    }
+
+                }
+
+            } while (true);
+
+        }
+
+        static void SeachArtist(IOrderedEnumerable<SongArtist> artistsSortAsc, iTunesSearchManager search, Model model)
+        {
+            int i = 1;
+            foreach (var artist in artistsSortAsc)
+            {
+                Console.WriteLine($"{i++} Артист: {artist.ArtistName}");
+            }
+
+            Console.WriteLine("     Выберите Вашего артиста, указав порядковый номер из списка:(наберите текст и нажмите Enter)");
+
+            bool result = int.TryParse(Console.ReadLine(), out int numberArtist);
+            Console.WriteLine("     поиск...");
+            if (result == true)
+            {
+                long choiceArtistId = artistsSortAsc.ElementAt(numberArtist - 1).ArtistId;
+                string choiceArtistName = artistsSortAsc.ElementAt(numberArtist - 1).ArtistName;
+
+                try
+                {
+                    var albums = search.GetAlbumsFromSongAsync(choiceArtistName).Result.Albums.OrderBy(a => a.CollectionName);
+                    int y = 1;
+                    DateTime dateTime;
+
+                    var OldAlbums = model.Albums.Where(o => o.AlbumArtistID == choiceArtistId);
+                    model.Albums.RemoveRange(OldAlbums);
+
+                    Console.WriteLine($"На сервере iTunes артист {choiceArtistName} представлен альбомами: ");
+                    foreach (var album in albums)
+                    {
+                        DateTime.TryParse(album.ReleaseDate, out dateTime);
+                        Console.WriteLine($"{y++} " +
+                            $"{album.CollectionName}, дата релиза: {dateTime.ToShortDateString()}");
+                        model.Albums.Add(new Album { AlbumArtistID = choiceArtistId, ArtistName = choiceArtistName.ToUpper(), AlbumName = album.ArtistName, DateRelise = dateTime });
+                    }
+
+                    model.SaveChanges();
+                }
+
+                catch (Exception e)
+                {
+
+                    if (!CheckConnection())
+                    {
+
+                        Cache(choiceArtistName.ToUpper(), model);
+                    }
+                    else
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                }
+
+            }
+            else 
+                Console.WriteLine("     Введите порядковый номер: число!");
+        }
+
+
+        static void Cache(string Artist, Model db)
+        {
+            string inputArtistUpper = Artist.ToUpper();
+            var albumsFromDB = db.Albums.ToList().Where(a => a.ArtistName == inputArtistUpper);
+            int o = 1;
+            foreach (var albumFromDB in albumsFromDB)
+            {
+                Console.WriteLine($"{o++} {albumFromDB.AlbumName}, дата релиза: {albumFromDB.DateRelise.ToShortDateString()}");
+            }
+
+
+            if (albumsFromDB.Count() == 0) Console.WriteLine("     Ничего не нашли!");
+
+        }
+
+        static bool CheckConnection()
+        {
+            HttpWebRequest req = WebRequest.Create("https://music.apple.com") as HttpWebRequest;
+            HttpWebResponse rsp;
+
+            try
+            {
+                rsp = req.GetResponse() as HttpWebResponse;
+            }
+            catch (WebException e)
+            {
+                if (e.Response is HttpWebResponse) rsp = e.Response as HttpWebResponse;
+                else rsp = null;
+                Console.WriteLine("     Соединение с сервером iTunes потеряно..., но всё равно поищем его альбомы...");
+            }
+
+            if (rsp != null) return true;
+            else return false;
+        }
+
+    }
+}
